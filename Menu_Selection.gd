8ed0af158b4fd56b482fcc6f0c5ed131220442f8
Felix <felix.falkenhain@student.uni-siegen.de>
2022-06-12 00:13:30 +0200

    Soundfixed for all Szenes

--- Menu_Selection.gd
+++ Menu_Selection.gd
@@ -1,4 +1,6 @@
 extends Control
+onready var test3 : AudioStreamPlayer = get_node("AudioStreamPlayer")
+
 
 func _on_RetryGameButton_pressed():
 	pass
@@ -19,25 +21,29 @@ func _on_Button_pressed():
 	var test1 : Button = $SoundButton 
 	var texture = ImageTexture.new()
 	texture.set_flags(0)
-	var test3 : AudioStreamPlayer = get_node("AudioStreamPlayer")
+	#var test3 : AudioStreamPlayer = get_node("AudioStreamPlayer")
 	
 	
 	match counter:
 		1: 
 			texture = load("res://Ressources/images/MainMenu/Sprite_Sound_Low.png")
 			test1.set_button_icon(texture)
-			test3.volume_db = -24 
+			Options.volume = -24 
+			test3.volume_db = Options.volume
 			counter = 2
 		2:
 			texture = load("res://Ressources/images/MainMenu/Sprite_Sound_Disabled.png")
 			test1.set_button_icon(texture)
-			test3.playing = 0
+			Options.volumeDisabled = 0
+			test3.playing = Options.volumeDisabled
 			counter = 3
 		3:
 			texture = load("res://Ressources/images/MainMenu/Sprite_Sound_Loud.png")
 			test1.set_button_icon(texture)
-			test3.playing = 1
-			test3.volume_db = -12 
+			Options.volumeDisabled = 1
+			test3.playing = Options.volumeDisabled
+			Options.volume = -12
+			test3.volume_db = Options.volume 
 			counter = 1 
 	
 	
