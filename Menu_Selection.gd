d057fff98f711561b80e0484df54762ab204b36f
Darian Schneider <darianschneider@web.de>
2022-06-11 20:33:20 +0200

    THINGS

--- Menu_Selection.gd
+++ Menu_Selection.gd
@@ -5,5 +5,9 @@ func _on_StartGameButton_pressed():
 	get_tree().change_scene("res://mainScene.tscn")
 
 
+func _on_ThingsButton_pressed():
+	get_tree().change_scene("res://things.tscn")
+
+
 func _on_QuitGameButton_pressed():
 	get_tree().quit()
---
+++ things.gd
@@ -0,0 +1,19 @@
+extends Control
+
+onready var label : Label = $Label
+var size = 16
+var dynamic_font = DynamicFont.new()
+
+func _input(event):
+	if Input.is_key_pressed(KEY_SPACE):
+		size += 1
+		dynamic_font.font_data = load("res://Ressources/font/Savior1.ttf")
+		
+		dynamic_font.size = size
+		size += 2
+		dynamic_font.outline_size = 1
+		dynamic_font.outline_color = "#000000"
+		label.grow_horizontal = Control.GROW_DIRECTION_BOTH
+		label.grow_vertical = Control.GROW_DIRECTION_BOTH
+		
+		label.add_font_override("font", dynamic_font)
